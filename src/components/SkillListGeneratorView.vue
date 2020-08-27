caab8db40fcedb634e807fe6e7c22815aba19eb1
Blade <bladerboles@gmail.com>
2020-08-26 18:53:01 -0600

    add list of races with their skills, needs some work because the wiki is inconsistent in listing skills that are given to all characters

--- src/components/SkillListGeneratorView.vue
+++ src/components/SkillListGeneratorView.vue
@@ -2,6 +2,20 @@
   <div class="container">
     <div class="hello">Hello there {{ msg }}!</div>
     <p>Here's your list of skills: {{ skillsList }}</p>
+    <p>Here's a list of races with their skills:</p>
+    <ul class="playableRaces">
+      <li v-for="race in playableRaces" :key="race.name">
+        Race: {{ race.name }}
+        <br />
+        <br />
+        Skills:
+
+        <div v-for="(skill, index) in race.skills" :key="index">
+          {{ skill }}
+        </div>
+        <br />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,7 +27,101 @@ export default {
   },
   data() {
     return {
-      skillsList: 'dying'
+      skillsList: 'dying',
+      playableRaces: [
+        {
+          name: 'Human',
+          skills: ['Climbing', 'Food Preservation', 'Haggling', 'Swimming'],
+          notes: 'none'
+        },
+        {
+          name: 'Troll',
+          skills: [
+            'Athletics',
+            'Bridge Building',
+            'Climbing',
+            'Food Preservation',
+            'Gemology',
+            'Mining'
+          ],
+          notes: 'none'
+        },
+        {
+          name: 'High Elf',
+          skills: ['Climbing', 'Dodge', 'Listening', 'Literacy', 'Stealth'],
+          notes: 'none'
+        },
+        {
+          name: 'Gray Elf',
+          skills: ['Climbing', 'Dodge', 'Listening', 'Literacy', 'Stealth'],
+          notes: 'none'
+        },
+        {
+          name: 'Dark Elf',
+          skills: ['Alertness', 'Climbing', 'Find Weakness', 'Stealth'],
+          notes: 'none'
+        },
+        {
+          name: 'Dwarf',
+          skills: [
+            'Climbing',
+            'Detect Traps',
+            'Metallurgy',
+            'Mining',
+            'Smithing'
+          ],
+          notes: 'none'
+        },
+        {
+          name: 'Gnome',
+          skills: [
+            'Climbing',
+            'Gemology',
+            'Listening',
+            'Mining',
+            'Pick Pockets',
+            'Ventriloquism'
+          ],
+          notes: 'none'
+        },
+        {
+          name: 'Hurthling',
+          skills: [
+            'Archery',
+            'Cooking',
+            'Food Preservation',
+            'Gardening',
+            'Stealth'
+          ],
+          notes: 'none'
+        },
+        {
+          name: 'Orc',
+          skills: ['Backstabbing', 'Find Weakness', 'Metallurgy', 'Mining'],
+          notes: 'none'
+        },
+        {
+          name: 'Drakeling',
+          skills: ['Alertness', 'Food Preservation', 'Music', 'Swimming'],
+          notes: 'none'
+        },
+        {
+          name: 'Mist Elf',
+          skills: [
+            'Alertness',
+            'Concentration',
+            'Dodge',
+            'Literacy',
+            'Necromancy'
+          ],
+          notes: 'none'
+        },
+        {
+          name: 'Ratling',
+          skills: ['Appraising', 'Detect Item Status', 'Survival', 'Swimming'],
+          notes: 'none'
+        }
+      ]
     }
   }
 }
