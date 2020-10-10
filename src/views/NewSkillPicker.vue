9152f368477547423a5847582da218c54af15d8c
Blade <bladerboles@gmail.com>
2020-10-10 11:32:44 -0600

    add new file, add fort awesome icons through npm

---
+++ src/views/NewSkillPicker.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="double-columns-container">
+    <ul class="column-ul">
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+    </ul>
+    <ul class="column-ul">
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+      <li class="column-li"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewSkillsPicker'
+}
+</script>
+
+<style scoped>
+.double-columns-container {
+  display: flex;
+  flex-direction: row;
+}
+
+ul {
+  list-style-type: none;
+  padding-left: 0;
+}
+</style>
