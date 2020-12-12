b7066fd13ada82aa41f04c542972949aff31f162
Blade <bladerboles@gmail.com>
2020-12-12 14:43:13 -0700

    rename and remove old skill picker from app, replace SkillPicker with NewSkillPicker

--- src/views/SkillPicker.vue
+++ src/views/SkillPicker.vue
@@ -1,172 +1,100 @@
 <template>
-  <div>
-    <p>
-      Bards start with random skills and merchants have slightly different
-      skills depending on specialty.
-    </p>
-    <p>
-      All characters train in listening, haggling, first aid and climbing so
-      they are not included.
-    </p>
-    <p>
-      Some skills can only be obtained in game, so these are not included in
-      this list.
-    </p>
-    <div class="skill-picker">
-      <div class="choices-div">
-        <div
-          v-for="skill in skillsList"
-          :key="skill.name + 'div'"
-          class="checkbox-div"
-        >
-          <input
-            @click="event => skillSelected(event)"
-            :key="skill.name + 'input'"
-            type="checkbox"
-            :value="skill.name"
-          />
-          <label :key="skill.name + 'label'" for="checkbox">{{
-            skill.name
-          }}</label>
-        </div>
+  <div class="double-columns-container">
+    <form class="skill-picker-form" submit.prevent="submit">
+      <div class="columns-component-div">
+        <SkillPickerColumn @skill-added="updateWantedSkills($event)" />
       </div>
-      <div class="combo-list-div">
-        <ul>
-          <li v-for="combo in this.possibleCombos" :key="combo + Math.random()">
-            {{ combo }}
-          </li>
-        </ul>
-      </div>
-    </div>
+
+      <CalculateCombosButton @click="calculateCombos"
+        >Calculate Combos</CalculateCombosButton
+      >
+    </form>
+    <PossibleCombosList :combosList="possibleCombos" />
   </div>
 </template>
 
 <script>
+import SkillPickerColumn from '../components/SkillPickerColumn'
+import CalculateCombosButton from '../components/CalculateCombosButton'
+import PossibleCombosList from '../components/PossibleCombosList'
 import {
   raceObjects,
   playableRaces,
   playableClasses,
-  skillsList
+  skillsList,
+  allCombinations
 } from '../tables/skills.js'
+
 export default {
-  name: 'SkillsPicker',
+  name: 'NewSkillsPicker',
+  components: { SkillPickerColumn, CalculateCombosButton, PossibleCombosList },
   data() {
     return {
-      raceObjects,
-      playableRaces,
-      playableClasses,
-      skillsList,
       wantedSkills: [],
+      allCombos: [],
       possibleCombos: []
     }
   },
   methods: {
+    updateWantedSkills(newSkillsList) {
+      this.wantedSkills = newSkillsList
+    },
+    calculateCombos() {
+      this.findValidCombos(this.wantedSkills)
+    },
     skillSelected(event = {}) {
-      console.log(event.target.value)
       let skillIndex = this.wantedSkills.indexOf(event.target.value)
-      console.log(skillIndex)
       if (skillIndex === -1) this.wantedSkills.push(event.target.value)
       else {
         this.wantedSkills.splice(skillIndex, 1)
       }
 
       this.possibleCombos = this.findValidCombos(this.wantedSkills)
-      console.log(this.possibleCombos)
     },
-    findValidCombos(desiredSkills = {}) {
+    findValidCombos(desiredSkills = []) {
       try {
-        console.log('yo')
-        // get desired skills list in an array
-
-        // for the first skill, calculate every possible combo and store it in an array
-        let firstSkillName = desiredSkills.slice(0, 1)[0]
-        console.log(firstSkillName)
-
-        let firstSkillObject = {}
-
-        firstSkillObject = this.skillsList.find(
-          skillName => firstSkillName === skillName.name
-        )
-        let possibleCombos = []
-        let newCompareArray = []
-        let answerArray = []
-
-        firstSkillObject.races.forEach(race => {
-          possibleCombos.push(...this.allCombosForRace(race))
+        const comboPossibilities = []
+        this.allCombos.forEach(combination => {
+          if (
+            desiredSkills.every(skill =>
+              combination.skills.includes(skill.name)
+            )
+          ) {
+            comboPossibilities.push(combination)
+          }
         })
-        firstSkillObject.classes.forEach(playerClass =>
-          possibleCombos.push(...this.allCombosForClass(playerClass))
-        )
-
-        desiredSkills.forEach(skill => {
-          let skillObject = this.skillsList.find(
-            skillName => skill === skillName.name
-          )
-          let allRaceCombos = []
-          skillObject.races.forEach(race => {
-            allRaceCombos = [...this.allCombosForRace(race), ...allRaceCombos]
-          })
-          let allClassCombos = []
-          skillObject.classes.forEach(playerClass => {
-            allClassCombos = [
-              ...this.allCombosForClass(playerClass),
-              ...allClassCombos
-            ]
-          })
-          possibleCombos = possibleCombos.filter(combo => {
-            return allRaceCombos.concat(allClassCombos).indexOf(combo) !== -1
-          })
-        })
-        return possibleCombos
+        this.possibleCombos = comboPossibilities
       } catch (error) {
         console.log('error: ', error)
       }
-
-      // for each other skill, calculate every combo but only add it to the array if it already exists
-
-      // return the finished possibilities
-
-      console.log(desiredSkills)
     },
-    allCombosForRace(playableRace) {
-      let allCombos = []
-      this.playableClasses.forEach(playableClass =>
-        // allCombos.push({
-        //   playableRace,
-        //   playableClass,
-        //   comboName: `${playableRace} ${playableClass}`
-        // })
-        {
-          if (playableRace !== 'None')
-            allCombos.push(`${playableRace} ${playableClass}`)
-        }
-      )
-
-      return allCombos
-    },
-    allCombosForClass(playableClass) {
-      let allCombos = []
-      this.playableRaces.forEach(playableRace =>
-        // allCombos.push({
-        //   playableRace,
-        //   playableClass,
-        //   comboName: `${playableRace} ${playableClass}`
-        // })
-        allCombos.push(`${playableRace} ${playableClass}`)
-      )
-      return allCombos
-    },
-
     removeDuplicateStrings(array) {
       return array.filter((a, b) => array.indexOf(a) === b)
     }
   },
-  mounted() {}
+  mounted() {
+    this.allCombos = allCombinations()
+  }
 }
 </script>
+
 <style scoped>
-div.skill-picker {
+.double-columns-container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  align-items: center;
+}
+
+.columns-component-div {
+  display: flex;
+  justify-content: center;
+}
+
+.skill-picker-form {
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 400px;
+  justify-content: space-between;
 }
 </style>
